d1d03e0130f8f792b7df9dbd1c525a1d3d4900fd
huenguyenhp2284-gif <huenguyenhp2284@gmail.com>
2026-03-27 19:39:04 +0700

    Add files via upload
    upload các bài buổi số

---
+++ bai2.js
@@ -0,0 +1,37 @@
+
+
+let name1 = "duke"
+let winners= 2
+let isEligible = false
+
+// console.log cho3 bieens treen laf xong//
+console.log(name1)
+console.log(winners)
+console.log(isEligible)
+let tiSoPhanTram // qt datwj teen laf camecase 2 ghi thuongwf conf laij ghi hoa
+
+
+let str1 = "xin chào"
+let str2 = "lop JSA24"
+
+// Gộp 2 chuỗi này lại
+let gop =str1 + str2
+console.log(gop)
+// Ở vị trí thứ 8 và 15 thì kết quả gộp là chữ gì?
+console.log(gop[8])
+console.log(gop[15])
+//In hoa tất cả các chuỗi
+console.log(gop.toUpperCase())
+//In thường tất cả các chuỗi
+console.log(gop.toLocaleLowerCase())
+
+let trai_cay = " Táo , cam , nho , chuối , quýt"
+// Sủ dụng split tách thành 1 danh sách traiCay
+let traiCay = trai_cay.split
+console.log(traiCay)
+
+let str3 ="toi"
+let str4 ="Ten là kevin"
+// Gộp lại str3 và  có khoảng cách ở giưa
+let gop2 =str3 + " "  + str4
+console.log(gop2)
